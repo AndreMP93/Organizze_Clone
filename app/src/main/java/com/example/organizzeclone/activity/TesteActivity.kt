aaf40478870663d20d04b8ed75ddf4875b527078
equipe711 <equipe711@inova.serpro.gov.br>
2022-06-08 18:41:42 -0300

    Atualização 08/06/2022 Configuração FloatingActionMenu

--- app/src/main/java/com/example/organizzeclone/activity/TesteActivity.kt
+++ app/src/main/java/com/example/organizzeclone/activity/TesteActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.organizzeclone.R
+import com.github.clans.fab.FloatingActionButton
+import com.github.clans.fab.FloatingActionMenu
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -12,6 +15,8 @@ class TesteActivity : AppCompatActivity() {
 
     private lateinit var botaoSair: Button
     private lateinit var autenticacao: FirebaseAuth
+    private lateinit var fabDespesa: FloatingActionButton
+    private lateinit var fabReceita: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,24 @@ class TesteActivity : AppCompatActivity() {
 
             autenticacao.signOut()
             finish()
+        }
+
+        fabDespesa = findViewById(R.id.menu_despesa)
+        fabDespesa.setOnClickListener {
+            Snackbar.make(
+                findViewById(R.id.tela_teste),
+                "DESPESA",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+
+        fabReceita = findViewById(R.id.menu_receita)
+        fabReceita.setOnClickListener {
+            Snackbar.make(
+                findViewById(R.id.tela_teste),
+                "RECEITA",
+                Snackbar.LENGTH_LONG
+            ).show()
         }
     }
 }
